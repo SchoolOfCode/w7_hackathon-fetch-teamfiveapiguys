8d7d3a046846f150097899aa6fb587ea676e4f4f
TaniaY21 <taniay@hotmail.co.uk>
2021-05-08 15:48:09 +0100

    task completed =)

--- main.js
+++ main.js
@@ -10,22 +10,26 @@ async function getDadJoke() {
   const jokes = await response.json();
   console.log(jokes);
   currentJoke = jokes.joke;
-
   renderScreen(currentJoke);
 }
 
 function renderScreen(joke) {
   let h1 = document.querySelector("h1");
   h1.innerHTML = joke;
+  createList();
 }
 
-function addToList() {
-  let li = document.createElement("li");
+function createList() {
+  let listLength = Math.min(5, myJokes.length);
+  let ol = document.querySelector("ol");
+  ol.innerHTML = "";
+  for (let i = 0; i < listLength; i++) {
+    let li = document.createElement("li");
+    li.innerText = myJokes[i].joke;
+    ol.appendChild(li);
+  }
 }
 getDadJoke();
-
-let button = document.querySelector("button");
-button.addEventListener("click", getDadJoke);
 
 let rateButton = document.getElementById("rate");
 rateButton.addEventListener("click", saveRating);
@@ -33,21 +37,12 @@ rateButton.addEventListener("click", saveRating);
 function saveRating() {
   let input = document.querySelector("input");
   let score = input.value;
-  console.log(score);
   let jokeObject = {
     joke: currentJoke,
     score: score,
   };
   myJokes.push(jokeObject);
   myJokes.sort((a, b) => b.score - a.score);
-
-  // save rating to pass on to sorting function
+//   createList();
+  getDadJoke();
 }
-
-// step 1: get the dad joke
-// step 2: add button to get new joke
-// step 2: rank joke via text input
-//          - make sure its an int
-//          - bubble sort it
-//          - rerender list with new sorted array
-// step 3: add jokes to ul
